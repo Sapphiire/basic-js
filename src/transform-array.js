bdc2b3b3998396e0fa62e4133a9d14c122fec21d
Vadzim <Sapphiire@github.com>
2020-10-04 18:50:31 +0300

    Update transform-array.js
    #2

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) throw Exception;
   const control_sequences = [
     '--discard-next',
     '--discard-prev',
     '--double-next',
     '--double-prev'
   ]
-  for(let i = 0; i < arr.length; i++) {
+  let result = [];
+  for(let i = 0; i < arr.length; i++)
     switch(arr[i]) {
-      case control_sequences[0]: arr.splice(i--, 2);
-      break;
-      case control_sequences[1]: arr.splice(--i, 2);
-      break;
-      case control_sequences[2]: arr[i] = arr[i + 1];
-      break;
-      case control_sequences[3]: arr[i] = arr[i - 1];
-      break;
+      case control_sequences[0]:i++;break;
+      case control_sequences[1]:result.pop();break;
+      case control_sequences[2]:if(i < arr.length) result.push(arr[i + 1]); break;
+      case control_sequences[3]:if(i > 0) result.push(arr[i - 1]);break;
+      default: result.push(arr[i]);
     }
-  }
-  return arr;
+  return result;
 };
