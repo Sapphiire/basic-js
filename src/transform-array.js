af2fc1927465a065c676a071aa4b7ef3661d08b9
Vadzim <Sapphiire@github.com>
2020-10-04 18:29:34 +0300

    Update transform-array.js
    transform_array completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const control_sequences = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev'
+  ]
+  for(let i = 0; i < arr.length; i++) {
+    switch(arr[i]) {
+      case control_sequences[0]: arr.splice(i--, 2);
+      break;
+      case control_sequences[1]: arr.splice(--i, 2);
+      break;
+      case control_sequences[2]: arr[i] = arr[i + 1];
+      break;
+      case control_sequences[3]: arr[i] = arr[i - 1];
+      break;
+    }
+  }
+  return arr;
 };
