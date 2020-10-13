d6d037713a5ef79f34e28fd47a5f7498b3eacc51
Vadzim <Sapphiire@github.com>
2020-10-13 21:17:21 +0300

    ...

--- src/what-season.js
+++ src/what-season.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function getSeason(date) {
   if(typeof date != Date) throw Error;
   const month = date.getMonth()
-  const season = "";
+  let season;
   if(month == 12 && month <= 2) season = 'winter';
   else if(month >= 3 && month <= 5) season = 'spring';
   else if(month >= 6 && month <= 8) season = 'summer';
