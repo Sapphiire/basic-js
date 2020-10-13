80f86a9b77df18ddfee157389841b8e2b455a82d
Vadzim <Sapphiire@github.com>
2020-10-14 01:12:23 +0300

    ....

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,5 +2,5 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, {repeatTimes = 0, separator = '+', addition, additionRepeatTimes = 0, additionSeparator = '|'}) {
   const addition_str = (new Array(additionRepeatTimes || 1)).fill(`${addition}`).join(additionSeparator);
-  return (new Array(repeatTimes || 1)).fill(`${str}${addition_str}`).join(separator)
+  return (new Array(repeatTimes || 1)).fill(str + addition_str).join(separator)
 };
