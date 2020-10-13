30b0aa5085c49d5b5ab580b46c327cc6b1fcdb11
Vadzim <Sapphiire@github.com>
2020-10-13 22:50:22 +0300

    ...

--- src/what-season.js
+++ src/what-season.js
@@ -1,12 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if(!(date instanceof Date)) return false;
+  
+  throw new CustomError('Not implemented');
+  /* if(!(date instanceof Date)) return false;
   const month = date.getMonth()
   let season;
   if(month == 11 || month <= 1) season = 'winter';
   else if(month >= 2 && month <= 4) season = 'spring';
   else if(month >= 5 && month <= 7) season = 'summer';
   else if(month >= 8 && month <= 10) season = 'autumn';
-  return season;
+  return season; */
 };
