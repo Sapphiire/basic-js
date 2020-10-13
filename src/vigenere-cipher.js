0abe3e8a7cc543749744e2032762c948ed388416
Vadzim <Sapphiire@github.com>
2020-10-14 02:28:14 +0300

    ...

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  direct = true
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  constructor(boolean) {
+    this.direct = boolean
+  }
+
+  encrypt(message, key) {
+    if(message === undefined || key === undefined) throw Error;
+    const upper_key = key.toUpperCase().split('')
+    let idx = 0;
+    const result = message.toUpperCase().split('').reduce((accumulator, currentValue) => {
+      if(this.alphabet.includes(currentValue)) {
+        idx = idx == key.length ? 0 : idx
+        return accumulator + this.alphabet[(this.alphabet.indexOf(currentValue) + this.alphabet.indexOf(upper_key[idx++])) % 26]
+      }
+      else
+        return accumulator + currentValue;
+    }, "")
+    return !this.direct ? result : result.split('').reverse().join('')
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(message, key) {
+    if(message === undefined || key === undefined) throw Error;
+    const new_key = key.toUpperCase().split('').map(element => this.alphabet[26 - this.alphabet.indexOf(element)]).join('')
+    return this.encrypt(message, new_key)
   }
 }
 
