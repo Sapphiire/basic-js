adaaeceeaef7bbeeeca7e3e41d13d05d9516793b
Vadzim <Sapphiire@github.com>
2020-10-13 21:15:08 +0300

    ...

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,6 +2,6 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
   if(!Array.isArray(members)) return false;
-  return members.reduce((accumulator, value) => typeof value === 'string' ? accumulator + value[0].trim() : accumulator, "")
+  return members.reduce((accumulator, value) => typeof value === 'string' ? accumulator + value.trim()[0] : accumulator, "")
   .toUpperCase().split('').sort().join('')
 };
