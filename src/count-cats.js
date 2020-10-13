f2562e3a559f21a80efb7287bb3967a9ce76a50c
Vadzim <Sapphiire@github.com>
2020-10-13 19:58:22 +0300

    countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,11 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  let count = 0;
-  matrix.forEach(row => row.forEach(element => {
-    if(element == '^^') count++; 
-    return element
-  }
-    ));
-  return Number(count);
+  return matrix.reduce((acc1, cur1) => acc1 + cur1.reduce((acc2, cur2) => cur2 == '^^' ? acc2 + 1 : acc2, 0), 0)
 };
