1b8eeacb0aab993dca01928f25b88b67e489289c
Vadzim <Sapphiire@github.com>
2020-10-13 23:57:29 +0300

    ...

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if(!Array.isArray(arr)) throw Exception;
+  if(!Array.isArray(arr)) throw Error;
   const control_sequences = [
     '--discard-next',
     '--discard-prev',
@@ -13,11 +13,9 @@ module.exports = function transform(arr) {
     switch(arr[i]) {
       case control_sequences[0]:i++;break;
       case control_sequences[1]:result.pop();break;
-      case control_sequences[2]:if(i < arr.length) result.push(arr[i + 1]); break;
+      case control_sequences[2]:if(i < arr.length - 1) result.push(arr[i + 1]); break;
       case control_sequences[3]:if(i > 0) result.push(arr[i - 1]);break;
       default: result.push(arr[i]);
     }
   return result;
 };
-
-// < 3
