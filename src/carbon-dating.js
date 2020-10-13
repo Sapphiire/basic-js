9d5132f80a4a509bc50ffa721ce8916d3deb12c4
Vadzim <Sapphiire@github.com>
2020-10-13 23:32:33 +0300

    ...

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,14 +4,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  /* if(typeof sampleActivity != String) return false;
+  if(typeof sampleActivity != String) return false;
   const regex = /[0-9\.]/
   if(!regex.test(sampleActivity)) return false;
 
   const activity = parseFloat(sampleActivity)
   const k = Math.log(2)/HALF_LIFE_PERIOD
   const t = Math.log(MODERN_ACTIVITY/activity)/k
-  return Math.ceil(t) */
-  
-  throw new CustomError('Not implemented');
+  return Math.ceil(t)
 };
