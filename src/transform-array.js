0cb74c8b163859b5d0363638b2b609d70f7add12
Vadzim <Sapphiire@github.com>
2020-10-14 10:45:08 +0300

    ...

--- src/transform-array.js
+++ src/transform-array.js
@@ -11,7 +11,7 @@ module.exports = function transform(arr) {
   let result = [];
   for(let i = 0; i < arr.length; i++)
     switch(arr[i]) {
-      case control_sequences[0]:i++;break;
+      case control_sequences[0]:{if(i < arr.length - 2 && (arr[i + 2] == control_sequences[1] || arr[i + 2] == control_sequences[3]))i++; i++};break;
       case control_sequences[1]:result.pop();break;
       case control_sequences[2]:if(i < arr.length - 1) result.push(arr[i + 1]); break;
       case control_sequences[3]:if(i > 0) result.push(arr[i - 1]);break;
